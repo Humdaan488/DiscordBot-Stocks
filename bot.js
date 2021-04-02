d24ef97e11601fdcfc1c7a1923774093ba3ed1c2
Humdaan Javed <77771083+Humdaan488@users.noreply.github.com>
2021-04-02 14:24:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.login( "token");
-const Stocks = require("stocks.js"); //link bot.js with the API
-const stocks = new Stocks('API key'); // replace XXXX with your API Key
+client.login("TOKEN");
 
-console.log("Let's get this show on the road");
+const finnhub = require('finnhub');
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = "API KEY";
+const finnhubClient = new finnhub.DefaultApi()
 
-//client.on is to communciate with the discord server
+console.log("Lets get this show on the road");
+
 client.on("ready", readyDiscord)
 
 function readyDiscord()
@@ -14,46 +16,46 @@ function readyDiscord()
     console.log("😎😍");
 } //Authenticated Successfully
 
-// Listener -> event tag of whenever a message comes do this function
-// 
-//client.on("message", stockPrice) //the string in the first parameter is called an 'event'. 
-client.on("message", gotMessage) //And the whole line is called a 'listener'
-
-async function gotMessage(msg)
+client.on("message", stockPrice)
+function stockPrice(msg)
 {
-    //console.log(msg.content);
-    if(msg.channel.id == "811415383109140531" && msg.content === "Humm") //Spam Channel only
+    if ( msg.channel.id == "811415383109140531");
     {
-        msg.channel.send("Yoooo");
+        messageSplit = msg.content.split(" ");
+        if(messageSplit[0] == "check")
+        {
+            for(i = 1; i < messageSplit.length; i++)
+            {
+                //the API itself doesnt throw an error, so we don't need a try-catch method
+                try
+                {
+                    finnhubClient.quote(messageSplit[i], (error, data, response) => {
+                    console.log(data)
+                    stockInfo = JSON.stringify(data);
+                    msg.channel.send(stockInfo);
+                    });
+                }              
+                catch(err)
+                {
+                    msg.channel.send(`The symbol ${messageSplit[i]} is invalid.`, err);
+                }
+            }
+        } 
     }
 }
 
-async function stockPrice(msg)
+client.on ("message", companyNews)
+function companyNews (msg)
 {
-    console.log(msg.content);
-    if(msg.channel.id == "811415383109140531" && msg.content === "TSLA")
+    if ( msg.channel.id == "811415383109140531" && msg.content == "news")
     {
-        var options = { symbol: "TSLA", interval: "1min", amount: 3};
-        // var options = { symbol: "TSLA", interval: "1min", start: new Date('2021-02-17T00:00:00.000Z'), end: new Date('2021-02-16T00:01:00.000Z')};
-        var result = await stocks.timeSeries(options);
-        stockInfo = JSON.stringify(result); //this converts the data into a string
-        msg.channel.send(stockInfo);
-
+        // Basic financials
+            finnhubClient.companyBasicFinancials("AAPL", "margin", (error, data, response) => {
+            console.log(data)
+            newsInfo = JSON.stringify (data);
+            msg.channel.send (newsInfo)    
+        });
     }
 }
 
-//TO-DO (NOW COMLETED)
-//-All messages are logged twice
-//-Combine stockPrice and gotMessage into one method
-//-generalize the ticker 
-//  -require a way to authenticate validity of the ticker
-//-String manipulation to find the wanted data (close price)
-//-Figure out how the time intervals work (1min amount: 1 gives data from 12am)
 
-
-// // client.on ("getPrice", stockPrice );
-
-// // async function stockPrice ()   {
-// //     const data = await yahooStockPrices.getCurrentData('AAPL');
-// //     console.log(data); // { currency: 'USD', price: 132.05 };
-// // }
